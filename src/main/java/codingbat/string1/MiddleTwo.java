a20d1664526e9e44a627ce52f76d38a9d1ea8081
Dana <danasdd11@gmail.com>
2024-08-12 22:40:32 +0600

    String1/MiddleTwo

---
+++ src/main/java/codingbat/string1/MiddleTwo.java
@@ -0,0 +1,8 @@
+package codingbat.string1;
+
+public class MiddleTwo {
+    public String middleTwo(String str) {
+        int middleIndex = str.length() / 2 -1;
+        return str.substring(middleIndex, middleIndex +2);
+    }
+}
