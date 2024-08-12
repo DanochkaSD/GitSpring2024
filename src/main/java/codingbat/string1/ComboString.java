df0fa03fa082605f8ba31c459fbe814fda0be8f3
Dana <danasdd11@gmail.com>
2024-08-12 22:28:40 +0600

    String1/ComboString

---
+++ src/main/java/codingbat/string1/ComboString.java
@@ -0,0 +1,7 @@
+package codingbat.string1;
+
+public class ComboString {
+    public String comboString(String a, String b) {
+        return (a.length() > b.length()) ?  b + a + b : a + b +a;
+    }
+}
