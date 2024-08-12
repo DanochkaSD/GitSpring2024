95c16071d7e032c275fa4a713157bab475591c21
Dana <danasdd11@gmail.com>
2024-08-12 23:23:20 +0600

    String2/CatDog

---
+++ src/main/java/codingbat/string2/CatDog.java
@@ -0,0 +1,16 @@
+package codingbat.string2;
+
+public class CatDog {
+    public boolean catDog(String str) {
+        int countCat = 0;
+        int countDog = 0;
+        for (int i = 0; i < str.length() - 2; i++) {
+            if (str.substring(i, i + 3).equals("cat")) {
+                countCat++;
+            } else if (str.substring(i, i + 3).equals("dog")) {
+                countDog++;
+            }
+        }
+        return countCat == countDog;
+    }
+}
